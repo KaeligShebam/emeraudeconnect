6aa4caf969ddca9e20acdc3a6cf274146920c2ff
KaeligShebam <kaelig@shebam.fr>
2024-01-13 16:15:43 +0100

    Add category for the system traduction

--- src/Controller/Back/Setting/Translation/AddTranslationController.php
+++ src/Controller/Back/Setting/Translation/AddTranslationController.php
@@ -18,21 +18,33 @@ class AddTranslationController extends AbstractController
         try {
             $key = $request->request->get('key');
             $value = $request->request->get('value');
+            $category = $request->request->get('category');  // Ajout de la récupération de la catégorie
+    
+            if (empty($key) || empty($value) || empty($category)) {
+                throw new \InvalidArgumentException('Les données fournies ne sont pas valides.');
+            }
     
             // Load the current content of the YAML file
             $yamlFilePath = $this->getParameter('kernel.project_dir') . '/translations/validators.fr.yaml';
             $content = Yaml::parse(file_get_contents($yamlFilePath));
     
-            // Add the new key-value pair
-            $content[$key] = $value;
+            // Add the new key-value pair to the specified category
+            if (!isset($content[$category])) {
+                $content[$category] = [];
+            }
+    
+            $content[$category][$key] = $value;
     
             // Save the modifications to the YAML file
             file_put_contents($yamlFilePath, Yaml::dump($content));
     
             return new JsonResponse(['message' => 'Nouvelle traduction ajoutée avec succès.']);
+        } catch (\InvalidArgumentException $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 400);
         } catch (\Exception $e) {
-            return new JsonResponse(['error' => 'Une erreur inattendue s\'est produite lors de l\'ajout de la traduction.']);
+            return new JsonResponse(['error' => 'Une erreur inattendue s\'est produite lors de l\'ajout de la traduction.'], 500);
         }
     }
+    
     
 }
--- src/Controller/Front/Auth/UserRegisterController.php
+++ src/Controller/Front/Auth/UserRegisterController.php
@@ -3,18 +3,20 @@
 namespace App\Controller\Front\Auth;
 
 use App\Entity\User;
+use Symfony\Component\Yaml\Yaml;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\Front\Auth\Register\UserRegisterType;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserRegisterController extends AbstractController
-
 {
     private $translator;
 
@@ -26,7 +28,6 @@ class UserRegisterController extends AbstractController
     #[Route('/authentification/inscription', name: 'register_user', methods: ['GET', 'POST'])]
     public function register(Security $security, Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
-
         if ($security->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->redirectToRoute('homepage');
         }
@@ -36,24 +37,74 @@ class UserRegisterController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user->setRoles(['ROLE_USER']);
-            
-            $hashedPassword = $passwordHasher->hashPassword(
-                $user,
-                $form->get('password')->getData()
-            );
-            $user->setPassword($hashedPassword);
-            $entityManager->persist($user);
-            $entityManager->flush();
+        $successMessage = null;
+        $errorMessage = null;
+        
 
-            $successMessage = $this->translator->trans('success_message_register_user', [], 'validators');
-        } else {
-            $successMessage = null;
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $user->setRoles(['ROLE_USER']);
+
+                $hashedPassword = $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                );
+                $user->setPassword($hashedPassword);
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $successMessage = $this->translator->trans('success_message_register_user', [], 'validators');
+            } catch (UniqueConstraintViolationException $e) {
+                // Gérer l'exception de contrainte d'unicité (doublon d'e-mail)
+                $errorMessage = $this->findTranslation('email_already_used_message');
+
+                // Utilisez un message d'erreur par défaut si la clé n'est pas trouvée
+                if ($errorMessage === null) {
+                    $errorMessage = $this->findTranslation('error_generic_message');
+                }
+            } catch (\Exception $e) {
+                // Gérer d'autres exceptions, si nécessaire
+                $errorMessage = $this->findTranslation('error_generic_message');
+            }
+            
         }
+
         return $this->render('/front/auth/register/user.html.twig', [
             'form' => $form->createView(),
-            'successMessage' => $successMessage
+            'successMessage' => $successMessage,
+            'errorMessage' => $errorMessage,
         ]);
+    }
+
+    private function findTranslation(string $key): ?string
+    {
+        // Chemin vers le fichier de traduction YAML
+        $yamlFilePath = $this->getParameter('kernel.project_dir') . '/translations/validators.fr.yaml';
+
+        // Charger le fichier YAML
+        $yamlContent = file_get_contents($yamlFilePath);
+        $translations = Yaml::parse($yamlContent);
+
+        // Recherche de la clé dans le tableau multidimensionnel
+        $result = $this->searchKeyInArray($key, $translations);
+
+        return $result;
+    }
+
+    private function searchKeyInArray(string $key, array $array): ?string
+    {
+        foreach ($array as $k => $v) {
+            if ($k === $key) {
+                return $v;
+            } elseif (is_array($v)) {
+                $result = $this->searchKeyInArray($key, $v);
+                if ($result !== null) {
+                    return $result;
+                }
+            }
+        }
+
+        return null;
     }
 }
