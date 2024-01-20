f5f2e767bbf506c1fb4a115be2778a8d3c6967a5
KaeligShebam <kaelig@shebam.fr>
2024-01-20 19:56:12 +0100

    Create an system edit page

--- src/Controller/Back/Page/EditController.php
+++ src/Controller/Back/Page/EditController.php
@@ -1,11 +1,10 @@
 <?php
+// src/Controller/Back/Page/EditController.php
 
 namespace App\Controller\Back\Page;
 
-use DateTime;
 use App\Entity\Page;
-use App\Repository\PageRepository;
-use App\Form\Back\Page\AddPageType;
+use App\Form\Back\Page\EditPageType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,33 +13,31 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/page')]
-class AddController extends AbstractController
+class EditController extends AbstractController
 {
-    #[Route('/ajouter', name: 'page_add_admin')]
-    public function new(Request $request, SluggerInterface $slugger, ManagerRegistry $doctrine): Response
+    #[Route('/modifier/{id}', name: 'admin_edit_page')]
+    public function edit(Request $request, Page $page, SluggerInterface $slugger, ManagerRegistry $doctrine): Response
     {
-        $page = new Page();
-        $form = $this->createForm(AddPageType::class, $page);
+        $form = $this->createForm(EditPageType::class, $page);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             // Générez le slug à partir du titre
             $page->setSlug($page->getTitle(), $slugger);
 
-            // Définissez la date de création
-            $page->setCreatedAt(new DateTime());
+            // Mettez à jour la date de modification
+            $page->setUpdatedAt(new \DateTime());
 
             // Persistez l'entité dans la base de données
             $entityManager = $doctrine->getManager();
-            $entityManager->persist($page);
             $entityManager->flush();
 
             // Redirigez l'utilisateur vers une page de confirmation, ou où vous voulez
         }
 
-        return $this->render('back/page/new.html.twig', [
+        return $this->render('back/page/edit.html.twig', [
             'page' => $page,
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/Back/Page/EditPageType.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Form\Back\Page;
+
+use App\Entity\Page;
+use App\Entity\PageStatus;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class EditPageType extends AbstractType
+{
+    private $kernel;
+    private $translator;
+
+    // Inject the KernelInterface in the constructor
+    public function __construct(TranslatorInterface $translator, KernelInterface $kernel)
+    {
+        $this->translator = $translator;
+        $this->kernel = $kernel;
+    }
+
+    private function findTranslation(string $key): ?string
+    {
+        // Chemin vers le fichier de traduction YAML
+        $yamlFilePath = $this->getKernelProjectDir() . '/translations/validators.fr.yaml';
+
+        // Charger le fichier YAML
+        $yamlContent = file_get_contents($yamlFilePath);
+        $translations = Yaml::parse($yamlContent);
+
+        // Recherche de la clé dans le tableau multidimensionnel
+        $result = $this->searchKeyInArray($key, $translations);
+
+        return $result;
+    }
+
+    private function getKernelProjectDir(): string
+    {
+        // Use the KernelInterface to get the project directory
+        return $this->kernel->getProjectDir();
+    }
+
+    private function searchKeyInArray(string $key, array $array): ?string
+    {
+        foreach ($array as $k => $v) {
+            if ($k === $key) {
+                return $v;
+            } elseif (is_array($v)) {
+                $result = $this->searchKeyInArray($key, $v);
+                if ($result !== null) {
+                    return $result;
+                }
+            }
+        }
+
+        return null;
+    }
+    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $buttonLabel = $this->findTranslation('btn_modify_page');
+    
+        $builder
+            ->add('title', TextType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-input', 'placeholder' => 'Titre de la page']
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+                'required' => false,
+                'attr' => ['class' => 'ckeditor form-input mt-2 tinymce']
+            ])
+            ->add('status', EntityType::class, [
+                'class' => PageStatus::class,
+                'label' => 'Statut de la page',
+                'choice_label' => 'name',
+                'attr' => ['class' => 'form-input mt-2'],
+                'label_attr' => [ 'class' => 'mt-2']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => $buttonLabel,
+                'attr' => ['class' => 'form-button mt-2']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Page::class,
+        ]);
+    }
+
+}
